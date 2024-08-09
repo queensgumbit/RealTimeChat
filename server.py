2889b9d1e584a8b7a03c98a48f901bbd9486b810
JasmineD <142790268+queensgumbit@users.noreply.github.com>
2024-08-09 11:45:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket
 import threading
+import chat_pb2  # Import the generated protobuf classes
 
-SERVER = '192.168.1.106'
+from client import FORMAT
+
+SERVER = '0.0.0.0'
 PORT = 5050
 HEADER = 64
-FORMAT = 'utf-8'
 DISCONNECT_MSG = 'disconnect!'
 
 class ClientConnection:
@@ -17,23 +19,19 @@ ADDR = (SERVER, PORT)
 connected_clients = []
 
 def broadcast(message, sender_client_socket):
-    print(f"[BROADCASTING] Broadcasting message: {message}")
     for client in connected_clients:
         if client.client_socket != sender_client_socket:
             try:
-                message_length = len(message)
-                send_length = str(message_length).encode(FORMAT)
-                send_length += b' ' * (HEADER - len(send_length))
-                client.client_socket.send(send_length)
-                client.client_socket.send(message.encode(FORMAT))
-                print(f"[SENT] Message sent to {client.nickname}")
+                client.client_socket.send(message)
             except Exception as e:
                 print(f"[ERROR] Failed to send message to {client.nickname}: {e}")
 
 def client_handler(client_socket, addr):
     try:
-        client_name_length = client_socket.recv(HEADER).decode(FORMAT)
-        client_name = client_socket.recv(int(client_name_length)).decode(FORMAT)
+        nickname_length = client_socket.recv(HEADER)
+        client_hello = chat_pb2.ClientHello()
+        client_hello.ParseFromString(nickname_length)
+        client_name = client_hello.nickname
         new_client = ClientConnection(client_socket, client_name)
         connected_clients.append(new_client)
         print(f'[CONNECTED] {client_name} is connected.')
@@ -43,13 +41,16 @@ def client_handler(client_socket, addr):
             try:
                 msg_length = client_socket.recv(HEADER).decode(FORMAT)
                 if msg_length:
-                    msg_length = int(msg_length)
-                    msg = client_socket.recv(msg_length).decode(FORMAT)
+                    msg_length = int(msg_length.strip())
+                    msg_data = client_socket.recv(msg_length)
+                    client_msg = chat_pb2.ClientSendMsg()
+                    client_msg.ParseFromString(msg_data)
+                    msg = client_msg.msg
                     if msg == DISCONNECT_MSG:
                         connected = False
                     else:
                         print(f"[{client_name}] : {msg}")
-                        broadcast(msg, client_socket)
+                        broadcast(msg_data, client_socket)
             except Exception as e:
                 print(f"[ERROR] {e}")
                 connected = False
@@ -58,7 +59,7 @@ def client_handler(client_socket, addr):
         client_socket.close()
         print(f'[DISCONNECTED] {client_name} ({addr}) disconnected.')
 
-def bindServer():
+def bind_server():
     try:
         server.bind(ADDR)
         print(f'[RUN] Running the server {SERVER} {PORT}')
@@ -76,7 +77,7 @@ def start_listening():
         print(f"\n[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 def main():
-    bindServer()
+    bind_server()
     start_listening()
 
 if __name__ == '__main__':
